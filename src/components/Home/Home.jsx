32a7cdd87824b8038ea694a18ae78625596fc995
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-11 18:35:17 -0500

    Added Image to home screen

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
+import think from './think.png'
 import './Home.css';
 
 function Home(){
 
     return(
         <>
-
+            <img src={think} alt="'Everybody in this country should learn how to program a computer, because it
+            teaches you how to think' by Steve Jobs"/>
         </>
     )
 }
